383236b3cda69730efecb658448a7d127d234405
Rodrigo Pérez <rodrigop_23@outlook.es>
2024-08-21 20:48:37 -0500

    feature: toggle dark and light theme in clerk components

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,11 +3,20 @@
 import ToggleTheme from "@/components/toggle-theme";
 import { SignIn } from "@clerk/nextjs";
 
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
+
 export default function Page() {
+  const { theme } = useTheme();
+
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="relative flex flex-row">
-        <SignIn />
+        <SignIn
+          appearance={{
+            baseTheme: theme === "dark" ? dark : undefined,
+          }}
+        />
 
         <div className="absolute top-3 right-3 z-50">
           <ToggleTheme />
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import ToggleTheme from "@/components/toggle-theme";
 import { SignUp } from "@clerk/nextjs";
 
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
+
 export default function Page() {
+  const { theme } = useTheme();
+
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="relative flex flex-row">
-        <SignUp />
+        <SignUp
+          appearance={{
+            baseTheme: theme === "dark" ? dark : undefined,
+          }}
+        />
 
         <div className="absolute top-3 right-3 z-50">
           <ToggleTheme />
